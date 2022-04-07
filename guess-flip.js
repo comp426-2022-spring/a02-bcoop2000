39553d49f8d799d1e83a82b86779806e7b480ab2
bcoop <bcoop@live.unc.edu>
2022-04-07 14:57:20 -0400

    changed order of if else statements

--- guess-flip.js
+++ guess-flip.js
@@ -4,17 +4,20 @@ import { flipACoin } from "./modules/coin.mjs";
 
 import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
-let call = args['call'] || 1;
+let call = args['call'] || 42;
 // import yargs from 'yargs';
 // const args = yargs(process.argv.slice(2)).argv;
 // let call = args.call;
 // check for errors
-if (call == 1) {
+if (call == 42) {
     console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
-if (call != "heads" && call != "tails") {
-    console.error("Usage: node guess-flip.js --call=[heads|tails]");
-} else {
+if (call == "heads") {
+    console.log(flipACoin(call));
+    
+} else if (call == "tails") {
     console.log(flipACoin(call))
+} else {
+    console.error("Usage: node guess-flip.js --call=[heads|tails]");
 }
 // Call the coinFlips function and put the return into STDOUT
